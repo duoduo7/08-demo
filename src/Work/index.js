31823e59aeece79b82873092581c5d31cde12399
duoduo7 <xiaohaierli@sina.cn>
2016-12-01 16:08:30 +0800

    aa

--- src/Work/index.js
+++ src/Work/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import echarts from 'echarts'
+
 
 class Work extends React.Component {
+  componentDidMount(){
+    var myChart = echarts.init(document.getElementById('main'));
+    myChart.setOption({
+    title: {
+      text: 'ECharts 入门示例' ,
+      textStyle:{
+        color:'pink'
+      },
+      left:'center'
+    },
+    tooltip: {},
+    xAxis: {
+        data: ["html","css","js","react","node","mongob"]
+
+    },
+    yAxis: {},
+    series: [{
+        name: '熟练度',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+    });
+  }
   render () {
+
     return(
-      <div>
-      work
+      <div id = 'main' style={{width: '100%',height:'auto'}}>
+
       </div>
     )
   }
